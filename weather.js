1d93f704e1bae9a5670f5c2fe0a94df03825656f
young_ee <taeyoung917@gmail.com>
2021-06-18 14:09:48 +0900

    t

--- weather.js
+++ weather.js
@@ -4,7 +4,7 @@ const COORDS = 'coords';
 function getweather(lat, lng){
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`);
 }
-
+console.log(latitude)
 function saveCoords (coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj))
 }
