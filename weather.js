8f96b2b75239ebfbea769670ae415fdf9d3beed4
young_ee <taeyoung917@gmail.com>
2021-06-18 15:42:11 +0900

    ddd

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ const COORDS = "coords";
 
 function getWeather(lat, lon) {
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
   )
     .then(function (reponse) {
       return reponse.json();
